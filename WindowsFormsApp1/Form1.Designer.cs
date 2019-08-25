421c7c8e3c1207dcdbd926fdc87434f07235e2f5
asrytikov@gmail.com <asrytikov@gmail.com>
2019-08-25 18:12:53 +0400

    commit 1

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(585, 411);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 442);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "База компонентов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string conn = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\ak\source\repos\WindowsFormsApp1\WindowsFormsApp1\Database1.mdf; Integrated Security = True";
+            sqlConnection = new SqlConnection(conn);
+            await sqlConnection.OpenAsync();
+
+            SqlDataReader sqlDataReader = null;
+            SqlCommand command = new SqlCommand("SELECT [dbo].tablsv.Id, [dbo].data.name, [dbo].data.count, [dbo].tablsv.parentId FROM [dbo].data INNER JOIN [dbo].tablsv ON [dbo].data.Id = [dbo].tablsv.elementId", sqlConnection);
+            try {
+                sqlDataReader = await command.ExecuteReaderAsync();
+                var nodes = new Dictionary<int, TreeNode>();
+                while (await sqlDataReader.ReadAsync())
+                {
+                    int id = sqlDataReader.GetInt32(0);
+                    int? parentId = sqlDataReader.IsDBNull(3) ? null : (int?)sqlDataReader.GetInt32(3);
+                    string name = sqlDataReader.GetString(1);
+                    
+
+                    var node = new TreeNode(name);
+                    TreeNode parentNode;
+                    if (parentId == null)
+                    {
+                        treeView1.Nodes.Add(node);
+                    }
+                    else { 
+                        if (nodes.TryGetValue(parentId.Value, out parentNode))
+                            parentNode.Nodes.Add(node);
+                    }
+                    nodes.Add(id, node);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally { if (sqlDataReader != null) {
+                    sqlDataReader.Close();
+                } }
+            sqlConnection.Close();
+        }
+    }
+}
